77e13a06db34c4ce041b65da29a79ba3f9582ae7
parjinder <parjinderpannu@gmail.com>
2025-03-03 13:20:42 -0800

    comments on test file

--- example_test.go
+++ example_test.go
@@ -15,3 +15,10 @@ func ExampleTokenize() {
 	// [who s on first]
 
 }
+
+/*
+Test discovery:
+For every file ending with _test.go, run every function that matches either:
+- Example[A-Z_].*, body must include // Output: Comment
+- Test[A-Z_].*
+*/
