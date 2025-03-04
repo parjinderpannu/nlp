a1da6cd040f15583d6d0cc559f15ceca3befa2e2
parjinder <parjinderpannu@gmail.com>
2025-03-04 09:45:27 -0800

    fuzzying on nlp test

--- nlp_test.go
+++ nlp_test.go
@@ -1,19 +1,22 @@
 package nlp
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/BurntSushi/toml"
 	"github.com/stretchr/testify/require"
 )
 
-// var tokenizeCases = []struct { // anonymous struct
-// 	text   string
-// 	tokens []string
-// }{
-// 	{"Who's on first?", []string{"who", "s", "on", "first"}},
-// 	{"", nil},
-// }
+/*
+var tokenizeCases = []struct { // anonymous struct
+	text   string
+	tokens []string
+}{
+	{"Who's on first?", []string{"who", "s", "on", "first"}},
+	{"", nil},
+}
+*/
 
 type tokenizeCase struct {
 	Text   string
@@ -33,11 +36,14 @@ func loadTokenizeCases(t *testing.T) []tokenizeCase {
 	// err = toml.Unmarshal(data, &testCases)
 	_, err := toml.DecodeFile("testdata/tokenize_cases.toml", &testCases)
 	require.NoError(t, err, "Unmarshal TOML")
-	// t.Log(testCases)
 	return testCases.Cases
 }
 
+// Exercise: Read test cases from tokenize_cases.toml
+// Use github.com/BurntSushi/toml to read TOML
+
 func TestTokenizeTable(t *testing.T) {
+	// for _, tc := range tokenizeCases {
 	for _, tc := range loadTokenizeCases(t) {
 		t.Run(tc.Text, func(t *testing.T) {
 			tokens := Tokenize(tc.Text)
@@ -48,13 +54,25 @@ func TestTokenizeTable(t *testing.T) {
 
 func TestTokenize(t *testing.T) {
 	text := "What's on second?"
-	expected := []string{"what", "", "on", "second"}
+	expected := []string{"what", "on", "second"}
 	tokens := Tokenize(text)
 	require.Equal(t, expected, tokens)
-	/* // Before testify
-	// if tokens != expected{ // Can't compare slices with == in go (only to nil)
+	/* Before testify
+	// if tokens != expected { // Can't compare slices with == in Go (only to nil)
 	if !reflect.DeepEqual(expected, tokens) {
 		t.Fatalf("expected %#v, got %#v", expected, tokens)
 	}
 	*/
+}
+
+func FuzzTokenize(f *testing.F) {
+	f.Fuzz(func(t *testing.T, text string) {
+		tokens := Tokenize(text)
+		lText := strings.ToLower(text)
+		for _, tok := range tokens {
+			if !strings.Contains(lText, tok) {
+				t.Fatal(tok)
+			}
+		}
+	})
 }
