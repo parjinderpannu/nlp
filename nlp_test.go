75319cc588c7e60a4dcbc73979b62ff04fe1933c
parjinder <parjinderpannu@gmail.com>
2025-03-03 18:49:29 -0800

    using testify require module

--- nlp_test.go
+++ nlp_test.go
@@ -1,16 +1,20 @@
 package nlp
 
 import (
-	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestTokenize(t *testing.T) {
 	text := "What's on second?"
 	expected := []string{"what", "s", "on", "second"}
 	tokens := Tokenize(text)
+	require.Equal(t, expected, tokens)
+	/* // Before testify
 	// if tokens != expected{ // Can't compare slices with == in go (only to nil)
 	if !reflect.DeepEqual(expected, tokens) {
 		t.Fatalf("expected %#v, got %#v", expected, tokens)
 	}
+	*/
 }
