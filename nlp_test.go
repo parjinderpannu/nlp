afd5728d6b214e2f41816f7ea5d2720d6a03bd91
parjinder <parjinderpannu@gmail.com>
2025-03-03 19:18:16 -0800

    test tokenize table

--- nlp_test.go
+++ nlp_test.go
@@ -6,6 +6,23 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var tokenizeCases = []struct { // anonymous struct
+	text   string
+	tokens []string
+}{
+	{"Who's on first?", []string{"who", "s", "on", "first"}},
+	{"", nil},
+}
+
+func TestTokenizeTable(t *testing.T) {
+	for _, tc := range tokenizeCases {
+		t.Run(tc.text, func(t *testing.T) {
+			tokens := Tokenize(tc.text)
+			require.Equal(t, tc.tokens, tokens)
+		})
+	}
+}
+
 func TestTokenize(t *testing.T) {
 	text := "What's on second?"
 	expected := []string{"what", "s", "on", "second"}
